641491146bb96693f9b9a08ec4888ddefd1e67c3
Konstantin Orlov <konstorlovrus@gmail.com>
2022-10-09 01:08:11 +0600

    first commit

---
+++ src/view/popup.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="main_app">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'popupView'
+}
+
+</script>
+
+<style>
+.main_app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  width: 400px;
+  height: 400px;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
